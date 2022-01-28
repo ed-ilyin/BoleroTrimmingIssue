f80c61765f02c6661adc588b640b7cfc1043c4c9
Ed Ilyin <Ed.Il@live.com>
2022-01-28 10:01:00 +0200

    adds DummyAuthProvider

--- src/Client/Startup.fs
+++ src/Client/Startup.fs
@@ -5,6 +5,19 @@ open Microsoft.Extensions.DependencyInjection
 open System
 open System.Net.Http
 open Microsoft.Extensions.Configuration
+open Microsoft.AspNetCore.Components.Authorization
+open Microsoft.AspNetCore.Components.WebAssembly.Hosting
+open Microsoft.Extensions.DependencyInjection
+open System.Security.Claims
+open System.Threading.Tasks
+
+type DummyAuthProvider() =
+    inherit AuthenticationStateProvider()
+
+    override _.GetAuthenticationStateAsync() =
+        let identity = ClaimsIdentity([|Claim(ClaimTypes.Name, "loic")|], "Fake auth type")
+        let user = ClaimsPrincipal(identity)
+        Task.FromResult(AuthenticationState(user))
 
 module Program =
 
@@ -16,10 +29,12 @@ module Program =
         builder.Services.AddScoped<HttpClient>(fun _ ->
             new HttpClient(BaseAddress = Uri builder.HostEnvironment.BaseAddress)) |> ignore
 
-        builder.Services.AddMsalAuthentication (fun options ->
-            builder.Configuration.Bind
-                ("AzureAdB2C", options.ProviderOptions.Authentication)
-        ) |> ignore
+        // builder.Services.AddMsalAuthentication (fun options ->
+        //     builder.Configuration.Bind
+        //         ("AzureAdB2C", options.ProviderOptions.Authentication)
+        // ) |> ignore
+        builder.Services.AddScoped<AuthenticationStateProvider, DummyAuthProvider>() |> ignore
+        builder.Services.AddAuthorizationCore() |> ignore
 
         builder.Build().RunAsync() |> ignore
         0
